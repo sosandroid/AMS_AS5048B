3e7e0f4a1fd0f2cd737bfa8ae02cabda8911b9dd
sosandroid <ehavet@yahoo.fr>
2014-10-03 11:19:53 +0200

    Bug fix

--- examples/read_simple_angle/read_simple_angle.ino
+++ examples/read_simple_angle/read_simple_angle.ino
@@ -14,6 +14,18 @@
 
 #include <ams_as5048b.h>
 
+//unit consts
+#define U_RAW 1
+#define U_TRN 2
+#define U_DEG 3
+#define U_RAD 4
+#define U_GRAD 5
+#define U_MOA 6
+#define U_SOA 7
+#define U_MILNATO 8
+#define U_MILSE 9
+#define U_MILRU 10
+
 AMS_AS5048B mysensor;
 
 void setup() {
@@ -37,10 +49,10 @@ void loop() {
 	//print to serial the raw angle and degree angle every 2 seconds
 	//print 2 times the exact same angle - only one measurement
 	Serial.print("Angle sensor raw : ");
-	Serial.println(mysensor.angleR("RAW", true), DEC);
+	Serial.println(mysensor.angleR(U_RAW, true), DEC);
 	
 	Serial.print("Angle degree : ");
-	Serial.println(mysensor.angleR("DEG", false), DEC);
+	Serial.println(mysensor.angleR(U_DEG, false), DEC);
 	
 	delay(2000);
 }
