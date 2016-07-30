1ef6cee418c0c0486af76f0bf8b3a3106d27557e
David Howlett <david.howlett3@gmail.com>
2016-07-30 15:24:58 +0100

    xplane example now works

--- examples/xplane_dials/xplane_dials.ino
+++ examples/xplane_dials/xplane_dials.ino
@@ -1,38 +1,35 @@
 #include <ams_as5048b.h>
 // authors: David Howlett, Peter Dobson, 
+
 // the constant U_DEG means the results are given in degrees
 #define U_DEG 3
 
-// declare an array of 4 sensors
-AMS_AS5048B sensors[4];
+// declare an array of sensors
+# define NUM_SENSORS 2
+AMS_AS5048B sensors[NUM_SENSORS];
+FlightSimFloat angles[NUM_SENSORS];
 
 void setup() {
   //Start serial
   Serial.begin(9600);
   while (!Serial) ; //wait until Serial ready
-  //init AMS_AS5048B objects
-  for(int i=0;i<4;i++){
-    //sensors[i]
+  //init AMS_AS5048B objects, each with it's own address
+  for(int i=0;i<NUM_SENSORS;i++){
+    sensors[i].chipAddress = 0x40+i;
     sensors[i].begin();
   }
 }
 
 void loop() {
-  /*
-  //print to serial the degree angle
-  knob0 = mysensor.angleR(U_DEG,true);
-  knob0Diff = knob0 - knob0Prev;
-  // If the knob0Diff is larger then 2 or less then -2
-  // then the change should be sent over serial. 
-  if (knob0Diff >= 10 || knob0Diff <= -10) {
-    Serial.print("  Angle deg : ");
-    Serial.print (knob0Diff);
-    Serial.print(" : ");
-    Serial.println (knob0);
-    knob0Prev = knob0;
+  for(int i=0;i<NUM_SENSORS;i++){
+    Serial.print("auto gain: ");
+    Serial.print(sensors[i].getAutoGain());
+    Serial.print(" angle: ");
+    angles[i] = sensors[i].angleR(U_DEG,true);
+    Serial.print(angles[i]);
+    Serial.print(' ');
   }
-  */
-
-  Serial.println(sensors[0].angleRegR());
-  delay (10);
+  Serial.print('\n');
+  FlightSim.update();
+  delay (5);
 }
